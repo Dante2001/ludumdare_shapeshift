fb25d9ede6b07534bb073cc2f9c8d99b6ecf8816
Matthew <matthew_palma37@live.ca>
2016-04-16 16:31:51 -0400

    added temp obs, audio swapping, speed up /down
    temp obstacles
    the audio that swaps when the player changes form
    audio speeds up when wolf eat sheep when human does taxes
    audio slows down if you hit wrong obstacle
    player speeds up when you hit the right obstacle
    player slows down when you hit the wrong obstacle
    right i've added a temp way to randomly spawn
    the obstacles

--- Assets/Scripts/MoveFloor.cs
+++ Assets/Scripts/MoveFloor.cs
@@ -6,8 +6,14 @@ public class MoveFloor : MonoBehaviour {
 	public int size;
 	GameObject goPlayer;
 	private int mount = 0;
+    
+    //temp stuff for testing purposes
+    public GameObject tHumanObs;
+    public GameObject tWerewolfObs;
+
 	// Use this for initialization
 	void Start () {
+
 		goPlayer = GameObject.Find ("Player");
 
 		mount = GameObject.FindGameObjectsWithTag (whichTag).Length;
@@ -18,6 +24,12 @@ public class MoveFloor : MonoBehaviour {
 	void Update () {
 		if (goPlayer.transform.position.x > this.transform.position.x+(size*(mount/2))){//+110) { 240
 			this.transform.position = new Vector3(this.transform.position.x + (mount*size),this.transform.position.y,this.transform.position.z);
+            
+            int temp = Random.Range(0,8);
+            if (temp == 0)
+                Instantiate(tHumanObs, new Vector3(this.transform.position.x, -2.88f, 0f), Quaternion.identity);
+            else if (temp == 7)
+                Instantiate(tWerewolfObs, new Vector3(this.transform.position.x, -2.88f, 0f), Quaternion.identity);
 		}
 
 	}
