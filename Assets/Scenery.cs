1a67d8d42ac6c0612c10df08be7f4834589b3601
Dante2001 <andrianodia@hotmail.com>
2016-04-16 17:37:21 -0300

    Foreground
    Added music
    Added foreground parallel
    Added foreground loop
    Added ortho compatibility

---
+++ Assets/Scenery.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scenery : MonoBehaviour {
+
+	int depth;
+	GameObject goPlayer;
+	float prevPosition;
+	// Use this for initialization
+	void Start () {
+		depth = GetComponent<SpriteRenderer> ().sortingOrder;
+		goPlayer = GameObject.Find ("Player");
+		prevPosition = goPlayer.transform.position.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3 (transform.position.x - (prevPosition - goPlayer.transform.position.x)/Mathf.Abs(depth), transform.position.y, transform.position.z);
+		prevPosition = goPlayer.transform.position.x;
+	}
+}
