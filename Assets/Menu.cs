e40a6aaf1200bf6a738fe3bd343f737f6d59205b
Dante2001 <andrianodia@hotmail.com>
2016-04-16 21:08:57 -0300

    Added Menu

---
+++ Assets/Menu.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+	public GameObject goMenu;
+	public GameObject goInstructions;
+	public GameObject goCredits;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void showCredits() {
+		goMenu.SetActive (false);
+		goInstructions.SetActive (false);
+		goCredits.SetActive (true);
+
+	}
+
+	public void showInstructions() {
+		goMenu.SetActive (false);
+		goInstructions.SetActive (true);
+		goCredits.SetActive (false);
+	}
+
+	public void goBack() {
+		goMenu.SetActive (true);
+		goInstructions.SetActive (false);
+		goCredits.SetActive (false);
+	}
+
+	public void startGame() {
+		Application.LoadLevel ("Game Dante");
+	}
+}
